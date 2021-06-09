111c641e7ec99f96adfa075e129663f4652e2151
AlegoS <quentin.alegos@gmail.com>
2021-06-09 14:02:52 +0200

    fix

--- src/app/models/Article_Promo.ts
+++ src/app/models/Article_Promo.ts
@@ -1,9 +1,14 @@
+import { Aliases } from "./models-ressources/Aliases";
+
 export class Article_Promo {
     public idArticle: number;
     public reduction: number;
 
     constructor(){}
-
+    private options = {
+        ConvivialsNames: Aliases.allergeneConvivialNames,
+        CustomRoutes: Aliases.allergeneCustomRoutes,
+    };
     public init_empty(): void{
         this.idArticle = 0;
         this.reduction = 0;
